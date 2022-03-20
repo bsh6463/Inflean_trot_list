590b7a30022b920f37adbcce891480e8900121b8
bsh6463 <bsh9510@gmail.com>
2022-03-21 00:38:08 +0900

    Initial commit

---
+++ app/src/main/java/com/example/trot_list/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.trot_list
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //1. Navigation만들기
+
+        //2. Fragment 만들고, nav연결
+            //2.1 fragment에서 이미지 누르면 이동.
+        //3. RecyclerView 만들기
+
+    }
+}
